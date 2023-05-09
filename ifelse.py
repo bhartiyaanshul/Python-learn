c53aaf8fec0eca0ff5bbcf3a12cfe200c9a09cdb
Anshul Bhartiya <bhartiyaanshul@gmail.com>
2023-05-09 23:09:59 +0530

    first commit

---
+++ ifelse.py
@@ -0,0 +1,7 @@
+a = int(input("Enter Your Budget 1.100 2.80 3.60 :"))
+if(a==1):
+    print("In your budget of 100 you can buy best quality of apple..")
+elif(a==2):
+    print("In your budget of 80 you can buy good quality of apple..")
+else:
+    print("In your budget of 100 you can buy medium quality of apple..")
