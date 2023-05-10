3d9f7fac266dea2cdbf5fbcd0317f41b34db47a1
Anshul Bhartiya <bhartiyaanshul@gmail.com>
2023-05-10 12:07:46 +0530

    str commit

---
+++ str.py
@@ -0,0 +1,16 @@
+a = "Anshul is good boy"
+print(len(a))
+print(a[5])
+print(a[::-1])
+print(a.count("s"))
+print(a.find("is"))
+print(a.upper())
+print(a.lower())
+print(a.capitalize())
+print(a.title())
+print(a.split())
+print(a.replace("good","best"))
+print(a.endswith("oy"))
+print(a.startswith("An"))
+      
+    
