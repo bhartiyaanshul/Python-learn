5b9d3e515104930246c39d08798cd93eb5df7f98
Anshul Bhartiya <bhartiyaanshul@gmail.com>
2023-05-10 17:02:47 +0530

    commit dic4

---
+++ dic4.py
@@ -0,0 +1,16 @@
+schl = {
+    "stud1" : {"Name":"Anshul",
+    "Id" : 1,
+    },
+    "stud2" : {
+        "Name": "Kashvi",
+        "Id": 2
+    }, 
+    "stud3": {
+        "Name": "Ridham",
+        "Id": 3
+    }      
+    }
+
+print(schl)
+
