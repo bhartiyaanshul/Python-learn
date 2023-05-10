bdbfae6b099fa2a86ad0c1e7368b4af37de57053
Anshul Bhartiya <bhartiyaanshul@gmail.com>
2023-05-10 14:46:45 +0530

    commit dic2

---
+++ dic2.py
@@ -0,0 +1,14 @@
+car = {"Brand": "Ford",
+       "Model":"Mustang",
+       "Color": "Black Matt",
+       "Year": 1998}
+
+for x in car.keys():
+    print(x)
+    
+for x in car.values():
+    print(x)
+    
+for x, y in car.items():
+    print(x, y)
+    
