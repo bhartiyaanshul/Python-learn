958f82d92080dfb4fd8ccb021eb74b39aba00219
Anshul Bhartiya <bhartiyaanshul@gmail.com>
2023-05-11 18:09:23 +0530

    commit tuple

---
+++ tupple.py
@@ -0,0 +1,12 @@
+car = ("Mustang", "Maybach", "AMG")
+
+# if "AMG" in car:
+#   print("yess")
+# c = list(car)
+# c.append("civic")
+# c.remove("civic")
+# car = tuple(c)
+
+for x in car:
+    print(x)
+
