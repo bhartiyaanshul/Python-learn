ea96bb13055648c40ded3ee9c346b70181dea5d0
Anshul Bhartiya <bhartiyaanshul@gmail.com>
2023-05-11 22:28:37 +0530

    commit array

---
+++ array_1.py
@@ -0,0 +1,8 @@
+
+n = int(input("Enter Number of Elements You Want :"))
+arr = []
+for x in range(n):
+    ele = int(input("Enter a Number:"))
+    arr.append(ele)
+for x in range(n):
+    print(arr[x])
