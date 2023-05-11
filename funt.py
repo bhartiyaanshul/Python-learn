5fd7cef5f61f7e10e0e8fcb1c639ba50849813c1
Anshul Bhartiya <bhartiyaanshul@gmail.com>
2023-05-11 18:13:20 +0530

    commit function

---
+++ funt.py
@@ -0,0 +1,12 @@
+# def food(food):
+#     for x in food:
+#         print(x)
+        
+# fastfood = ["Burger","Pizza","French Fries"]
+# food(fastfood)
+
+
+def multi(x):
+    return x * 5
+
+print(multi(5))
