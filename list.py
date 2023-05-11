55034cf810b6ff4f6adf5bc7f5076018cf7651a6
Anshul Bhartiya <bhartiyaanshul@gmail.com>
2023-05-11 12:11:23 +0530

    commit list

---
+++ list.py
@@ -0,0 +1,17 @@
+fruit = ["Apple","Banana","Kiwi","Cheery","Mango"]
+num = [100,34,76,45,89]
+fruitc = []
+# fruit2 = [ x for x in fruit if "a" in x]
+# for x in fruit:
+#     if "a" in x:
+#         fruit2.append(x)
+# fruit.sort()
+# fruit.sort(reverse= True)
+# num.sort()
+# num.sort(reverse=True)
+
+# fruitc = fruit.copy()
+
+fruitc = num + fruit
+    
+print(fruitc)
