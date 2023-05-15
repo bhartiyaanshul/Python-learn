8b24df5f9defe257c79677be10aa4d8ee8149ea8
Anshul Bhartiya <bhartiyaanshul@gmail.com>
2023-05-15 22:21:48 +0530

    commit pattern2.py

--- pattern2.py
+++ pattern2.py
@@ -48,17 +48,17 @@ for i in range(n-1):
     for j in range(i,n):
         print(" ",end=" ")
     for j in range(i):
-        print(j+1,end=" ")
+        print("*",end=" ")
     for j in range(i+1):
-        print(j+1,end=" ")
+        print("*",end=" ")
     print()
 for i in range(n):
     for j in range(i+1):
         print(" ",end=" ")
     for j in range(i,n):
-        print(j+1,end=" ")
+        print("*",end=" ")
     for j in range(i,n-1):
-        print(j+1,end=" ")
+        print("*",end=" ")
     
         
     print()
