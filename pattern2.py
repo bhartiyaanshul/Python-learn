e67d1ca54eda75f71a28f322f78d843863612267
Anshul Bhartiya <bhartiyaanshul@gmail.com>
2023-05-15 12:28:35 +0530

    commit pattern2

--- pattern2.py
+++ pattern2.py
@@ -8,12 +8,57 @@ n = int(input("Enter the lenght:"))
 #     print()
 #     i += 1
 
-i = 5
-while(i<=n):
-    j = 5
-    while(j<=i):
-        print("*",end=" ")
-        j += 1
+# i = 5
+# while(i<=n):
+#     j = 5
+#     while(j<=i):
+#         print("*",end=" ")
+#         j += 1
         
-        print()
-        i += 1
+#         print()
+#         i += 1
+
+# for i in range(n):
+#     print(" "*(n-i-1)+"* "*(i+1))
+# for j in range(n-1,0,-1):
+#     print(" "*(n-j)+"* "*(j))
+
+
+# for i in range(n):
+#     for j in range(i,n):
+#         print(" ",end=" ")
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     for j in range(i):
+#         print("*",end=" ")
+       
+#     print()
+
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ",end=" ")
+#     for j in range(i,n):
+#         print("*",end=" ")
+#     for j in range(i,n-1):
+#         print("*",end=" ") 
+        
+#     print()       
+for i in range(n-1):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i):
+        print(j+1,end=" ")
+    for j in range(i+1):
+        print(j+1,end=" ")
+    print()
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(i,n):
+        print(j+1,end=" ")
+    for j in range(i,n-1):
+        print(j+1,end=" ")
+    
+        
+    print()
