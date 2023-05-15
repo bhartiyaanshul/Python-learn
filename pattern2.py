00c7590858fdc7506dac27501c35dfa5caebd27b
Anshul Bhartiya <bhartiyaanshul@gmail.com>
2023-05-15 10:21:43 +0530

    commit pattern

---
+++ pattern2.py
@@ -0,0 +1,19 @@
+n = int(input("Enter the lenght:"))
+# i = 1
+# while(i<=n):
+#     j = 1
+#     while(j<=i):
+#         print("*",end=" ")
+#         j += 1
+#     print()
+#     i += 1
+
+i = 5
+while(i<=n):
+    j = 5
+    while(j<=i):
+        print("*",end=" ")
+        j += 1
+        
+        print()
+        i += 1
