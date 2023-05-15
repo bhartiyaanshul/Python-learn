9d2931a6a820281d4c64f4efd97d4be019ab3aea
Anshul Bhartiya <bhartiyaanshul@gmail.com>
2023-05-15 22:46:53 +0530

    commit continue

--- continue.py
+++ continue.py
@@ -1,4 +1,13 @@
-for n in range(1,11):
-    if n == 3 or n == 7:
+# for n in range(1,11):
+#     if n == 3 or n == 7:
+#         continue
+#     print(n)
+
+
+n = int(input("Enter a range:"))
+
+for i in range(1,n+1):
+    if i % 2 == 0:
         continue
-    print(n)
+    else:
+        print(i)
