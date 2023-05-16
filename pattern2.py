a03fdcbadb864921ed0c10e4ebc6e2221af8b7ca
Anshul Bhartiya <bhartiyaanshul@gmail.com>
2023-05-16 15:48:21 +0530

    commit pattern2

--- pattern2.py
+++ pattern2.py
@@ -1,4 +1,4 @@
-n = int(input("Enter the lenght:"))
+# n = int(input("Enter the lenght:"))
 # i = 1
 # while(i<=n):
 #     j = 1
@@ -44,21 +44,33 @@ n = int(input("Enter the lenght:"))
 #         print("*",end=" ") 
         
 #     print()       
-for i in range(n-1):
-    for j in range(i,n):
-        print(" ",end=" ")
-    for j in range(i):
-        print("*",end=" ")
-    for j in range(i+1):
-        print("*",end=" ")
-    print()
-for i in range(n):
-    for j in range(i+1):
-        print(" ",end=" ")
-    for j in range(i,n):
-        print("*",end=" ")
-    for j in range(i,n-1):
-        print("*",end=" ")
+# for i in range(n-1):
+#     for j in range(i,n):
+#         print(" ",end=" ")
+#     for j in range(i):
+#         print("*",end=" ")
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ",end=" ")
+#     for j in range(i,n):
+#         print("*",end=" ")
+#     for j in range(i,n-1):
+#         print("*",end=" ")
     
+        
+#     print()
+
+rows = 7
+cols = 7
+
+for row in range(11):
+    for col in range(11):
+        if (col == 0 and row < 5 ) or (col == 5) or (row == 5 and col != 5) or (row == 0 and col >=6) or (row > 5 and col == 10) or (row == 10 and col<5) or (col==2 and row == 2) or (col==8 and row== 2) or (col==2 and row== 7) or (col==8 and row== 7):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
         
     print()
