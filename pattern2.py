071690f1b92aee2e7a0c8e0f4660c9a5ad976e81
Anshul Bhartiya <bhartiyaanshul@gmail.com>
2023-05-22 10:41:06 +0530

    commit pattern2

--- pattern2.py
+++ pattern2.py
@@ -63,14 +63,41 @@
         
 #     print()
 
-rows = 7
-cols = 7
+# rows = 7
+# cols = 7
 
-for row in range(11):
-    for col in range(11):
-        if (col == 0 and row < 5 ) or (col == 5) or (row == 5 and col != 5) or (row == 0 and col >=6) or (row > 5 and col == 10) or (row == 10 and col<5) or (col==2 and row == 2) or (col==8 and row== 2) or (col==2 and row== 7) or (col==8 and row== 7):
-            print("*",end=" ")
-        else:
-            print(" ",end=" ")
+# for row in range(11):
+#     for col in range(11):
+#         if (col == 0 and row < 5 ) or (col == 5) or (row == 5 and col != 5) or (row == 0 and col >=6) or (row > 5 and col == 10) or (row == 10 and col<5) or (col==2 and row == 2) or (col==8 and row== 2) or (col==2 and row== 7) or (col==8 and row== 7):
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
         
-    print()
+#     print()
+
+# Size of the swastika (adjust as needed)
+size = 7
+
+# Create a 2D grid to represent the swastika
+swastika = [[' ' for _ in range(size)] for _ in range(size)]
+
+# Loop to fill the swastika pattern
+row = 0
+while row < size:
+    col = 0
+    while col < size:
+        if (
+            (row < size // 2 and col < size // 2 and col >= row) or
+            (row >= size // 2 and col >= size // 2 and col <= row) or
+            (row >= size // 2 and col < size // 2 and (size - col - 1) <= row) or
+            (row < size // 2 and col >= size // 2 and (size - row - 1) <= col)
+        ):
+            swastika[row][col] = '*'
+        col += 1
+    row += 1
+
+# Print the swastika
+for row in swastika:
+    print(' '.join(row))
+
+a=[1,2,
