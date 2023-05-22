3a9f290da5465e1ee4b9720ee85efe2e96cf37cc
Anshul Bhartiya <bhartiyaanshul@gmail.com>
2023-05-22 10:40:09 +0530

    commit atm

--- atm.py
+++ atm.py
@@ -15,30 +15,32 @@ balance = 0
 #         print("Your Balance after withdraw is ",balance)
 #     case 3:
 #         print("Your Balance is ",balance)
-while (True):
-    s = input("Enter Number for Following service \n1) Deposite \n2) Withdraw \n3) Balance: \n4) Exit ATM\n")
-    if 1 <= int(s) <= 4:
-            if s == 1:
-                if s >= 0 and s <= 9:
+try:
+    
+    while (True):
+            s = int(input("Enter Number for Following service \n1) Deposite \n2) Withdraw \n3) Balance: \n4) Exit ATM\n"))
+            try:
+                if s == 1:
                     deposit = int(input("Enter a amount to be deposit in your account: "))
                     balance += deposit
                     print("Your Balance after deposit is ", balance)
-                else:
-                    print("Enter a Valid Amount")
             
-            elif s == 2:
-                withdraw = int(input("Enter a amount you have to withdraw: "))
-                balance -= withdraw
-                print("Your Balance after withdraw is ", balance)
+                elif s == 2:
+                    withdraw = int(input("Enter a amount you have to withdraw: "))
+                    balance -= withdraw
+                    print("Your Balance after withdraw is ", balance)
 
-            elif s == 3:
-                print("Your Balance is ", balance)
-                
-            else:
-                print("You Have Exited the ATM Machine, Thank You")
-                break
-    else:
-        print("Enter a Valid Option..")
+                elif s == 3:
+                    print("Your Balance is ", balance)
+                            
+                else:
+                    print("You Have Exited the ATM Machine, Thank You")
+                    break
+            except:
+                print("Please Enter Numeric Value..")
+except:
+    print("Please select from the following option..")    
+
         
         
     
